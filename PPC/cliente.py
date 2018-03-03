053095f8bf53a495df5fbbf46fdf0c925b013b59
Gustavo.Soares <gustavo.soares.cdc@gmail.com>
2018-03-03 10:53:28 -0300

    commit

---
+++ PPC/cliente.py
@@ -0,0 +1,8 @@
+class Client:
+
+    def __init__(self, name, agency, account, phone, balance):
+        self.name = name
+        self.agency = agency
+        self.account = account
+        self.phone = phone
+        self.balance = balance
---
+++ PPC/main.py
@@ -0,0 +1,5 @@
+from cliente import Client
+
+c1 = Client("gustavo", "123", "456", "87798564", "100.00")
+
+print(c1.name)
