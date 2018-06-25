d13afe96332ea6e6cc2a4d2d4ba2c3469d81cd12
gustavo5855 <gustavo.soares.cdc@gmail.com>
2018-06-25 13:33:30 -0300

    commit

---
+++ ES/interface/matriz.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'matriz.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(517, 462)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(0, 160, 511, 241))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setRowCount(2)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsUserCheckable)
+        self.tableWidget.setItem(0, 0, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsUserCheckable)
+        self.tableWidget.setItem(1, 1, item)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(140, 10, 59, 18))
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(200, 10, 113, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(140, 70, 131, 20))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(270, 71, 41, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 517, 30))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        item = self.tableWidget.verticalHeaderItem(0)
+        item.setText(_translate("MainWindow", "critério 1"))
+        item = self.tableWidget.verticalHeaderItem(1)
+        item.setText(_translate("MainWindow", "critério 2"))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "critério"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "critério 2"))
+        __sortingEnabled = self.tableWidget.isSortingEnabled()
+        self.tableWidget.setSortingEnabled(False)
+        item = self.tableWidget.item(0, 0)
+        item.setText(_translate("MainWindow", "1"))
+        item = self.tableWidget.item(1, 1)
+        item.setText(_translate("MainWindow", "1"))
+        self.tableWidget.setSortingEnabled(__sortingEnabled)
+        self.label.setText(_translate("MainWindow", "Objetivo"))
+        self.label_2.setText(_translate("MainWindow", "Numero de critérios"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ ES/script.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from interface.matriz import Ui_MainWindow
+# import numpy as np
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+criterios = []
+
+# def matriz(qtd_criterios, matriz):
+#     for i in range(1, qtd_criterios+1+1):
+#         linhas = []
+#         for j in range(1, qtd_criterios+1+1):
+#             linhas.append(criterios[j-1])
+#         mat.append(linhas)
+#     return mat
+#
+#
+# if __name__ == "__main__":
+#     j=0
+#     objective = input("diga seu objetivo: ")
+#     qtd_criterios = int(input("Quantos criterios: "))
+#
+#     mat = np.zeros((qtd_criterios, qtd_criterios))
+#     np.fill_diagonal(mat, 1)
+#
+#     print("Critérios:")
+#     for i in range(0, qtd_criterios):
+#         j=j+i
+#     print("triangular inferior = ", j)
+#
+#     print(mat)
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    #------------------------------------
+
+    #------------------------------------
+    MainWindow.show()
+    # ui.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+    sys.exit(app.exec_())
